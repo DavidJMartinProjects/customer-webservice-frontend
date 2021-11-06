f7676ba007b1cbebf9b0657cdeb8f3bb867105c7
unknown <davidjmartin@hotmail.com>
2021-11-06 16:08:49 +0000

    extracted baseUrl to class variable

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -2,19 +2,21 @@ import { CustomerPage } from './../model/customer-page.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor(private httpClient: HttpClient) { }
+  baseUrl: string = 'http://localhost:8080/customers';
 
+  constructor(private httpClient: HttpClient) { }
+  
   getCustomerPage(pageNumber: number, pageSize: string): Observable<CustomerPage> {
-    console.debug("pageSize: " + pageSize + ", pageNumber: " + pageNumber);
-    var baseUrl = `http://localhost:8080/customers?page=${pageNumber - 1}&size=${pageSize}`
-    return this.httpClient.get<CustomerPage>(baseUrl).pipe();
+    console.debug("pageSize: " + pageSize + ", pageNumber: " + pageNumber);    
+    var query = `?page=${pageNumber - 1}&size=${pageSize}`;
+
+    return this.httpClient.get<CustomerPage>(this.baseUrl + query);
   }
   
 }
